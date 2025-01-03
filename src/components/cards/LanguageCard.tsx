12ec3fcdf84e57c850c35dafacd287ffe163d1b4
Jahidul Islam <jahidul.app@gmail.com>
2025-01-03 16:00:00 +0600

    feat: adding download option

--- src/components/cards/LanguageCard.tsx
+++ src/components/cards/LanguageCard.tsx
@@ -1,46 +1,113 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Image } from 'expo-image'
 import { fonts } from '@/styles'
 import { colors, fontSize } from '@/constants/token'
 import Download from '@/constants/icons/download'
+import { useDownloadFile } from '@/hooks/useDownloadFile'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import Play from '@/constants/icons/play'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
 
 export default function Card({ data }: { data: any }) {
+	const [downloadedUri, setdownloadedUri] = useState('')
+	// const { setCurrentTrack } = usePlayerContext()
+	const { handleDownloadFile, downloadProgress, uri } = useDownloadFile()
+
+	const getData = async () => {
+		const jsonValue = await AsyncStorage.getItem(`${data.id}`)
+		return jsonValue && jsonValue?.length > 0 ? setdownloadedUri(jsonValue) : setdownloadedUri('')
+	}
+
+	const setData = async () => {
+		await AsyncStorage.setItem(`${data.id}`, uri)
+	}
+
+	useEffect(() => {
+		getData()
+	}, [data])
+
+	useEffect(() => {
+		if (downloadProgress == 1) {
+			setData()
+			setdownloadedUri(uri)
+		}
+		console.log(downloadProgress)
+	}, [downloadProgress, uri])
+
 	return (
-		<View style={styles.container}>
+		<TouchableOpacity
+			activeOpacity={0.7}
+			disabled={downloadProgress > 0 && downloadProgress !== 100}
+			style={styles.container}
+			onPress={() => {
+				if (downloadedUri) {
+					let item = { ...data, uri: '' }
+					item.uri = downloadedUri
+					// setCurrentTrack(data)
+				} else {
+					if (downloadProgress > 0) return
+					handleDownloadFile({
+						url: data.fileUrl,
+						fileName: `${data?.place?.name}-${data?.language.name}.${data?.fileUrl.split('.').pop()}`,
+					})
+				}
+			}}
+		>
 			{/* left */}
 			<View style={styles.leftContainer}>
 				<Image style={styles.image} source={data?.language?.imgUrl} />
-				<Text style={[fonts.normal, {fontSize: fontSize.sm}]}>{data?.language?.name}</Text>
+				<Text style={[fonts.normal, { fontSize: fontSize.sm }]}>{data?.language?.name}</Text>
 			</View>
 
 			{/* right */}
 			<View>
-                <Download fill={colors.primary} />
-            </View>
-		</View>
+				{downloadedUri ? (
+					<View style={styles.button}>
+						<Play fill={colors.primary} size='20' />
+					</View>
+				) : downloadProgress == 0 ? (
+					<View style={styles.button}>
+						<Download fill={colors.primary} size='20' />
+					</View>
+				) : (
+					<AnimatedCircularProgress
+						size={20}
+						width={2}
+						fill={downloadProgress * 100}
+						tintColor={colors.primary}
+						backgroundColor={colors.muted}
+					/>
+				)}
+			</View>
+		</TouchableOpacity>
 	)
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: colors.muted,
-    },
-    leftContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 10
-    },
-    image: {
-        width: 40,
-        aspectRatio: 1
-    }
+	container: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 20,
+		paddingVertical: 5,
+		paddingHorizontal: 10,
+		borderWidth: 1,
+		borderRadius: 10,
+		borderColor: colors.muted,
+	},
+	leftContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 10,
+	},
+	image: {
+		width: 40,
+		aspectRatio: 1,
+	},
+	button: {
+		backgroundColor: colors.muted,
+		padding: 10,
+		borderRadius: 50,
+	},
 })
---
+++ src/hooks/useDownloadFile.ts
@@ -0,0 +1,63 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
+
+export function useDownloadFile() {
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [uri, setUri] = useState<string>("");
+
+  const callback = (downloadProgress: any) => {
+    const progress =
+      downloadProgress.totalBytesWritten /
+      downloadProgress.totalBytesExpectedToWrite;
+    setDownloadProgress(progress);
+  };
+
+  const handleDownloadFile = async ({
+    url,
+    fileName,
+  }: {
+    url: string;
+    fileName: string;
+  }) => {
+    const downloadResumable = FileSystem.createDownloadResumable(
+      url,
+      FileSystem.cacheDirectory + fileName,
+      {},
+      callback,
+    );
+    try {
+      const { uri } =
+        (await downloadResumable.downloadAsync()) as FileSystem.FileSystemDownloadResult;
+      setUri(uri);
+      console.log("Finished downloading to ", uri);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteFiles = async () => {
+    const allDataKeys = await AsyncStorage.getAllKeys();
+    console.log(allDataKeys);
+    const allData = await AsyncStorage.multiGet(allDataKeys);
+    try {
+      for (let i = 0; i < allData.length; i++) {
+        await FileSystem.deleteAsync(allData[i][1] as string);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      await AsyncStorage.clear();
+      setIsDeleted(true);
+    }
+  };
+
+  return {
+    handleDownloadFile,
+    downloadProgress,
+    uri,
+    handleDeleteFiles,
+    isDeleted,
+  };
+}
